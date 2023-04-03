59d6bb005b4661dea6fb1482d19602b2686ea7f0
d-hutchins <129690658+d-hutchins@users.noreply.github.com>
2023-04-02 20:59:30 -0400

    Add files via upload

---
+++ Prog3/Prog3/Prog3.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace Prog3
+{
+    partial class Prog3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.entreeNumberLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.initialCostLabel = new System.Windows.Forms.Label();
+            this.adjustedCostLabel = new System.Windows.Forms.Label();
+            this.shipmentCostLabel = new System.Windows.Forms.Label();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
+            this.cityComboBox = new System.Windows.Forms.ComboBox();
+            this.entreeNumberTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.initialCostOutputLabel = new System.Windows.Forms.Label();
+            this.adjustedCostOutputLabel = new System.Windows.Forms.Label();
+            this.shipmentCostOutputLabel = new System.Windows.Forms.Label();
+            this.totalPriceOutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Location = new System.Drawing.Point(308, 117);
+            this.cityLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(55, 25);
+            this.cityLabel.TabIndex = 0;
+            this.cityLabel.Text = "City:";
+            // 
+            // entreeNumberLabel
+            // 
+            this.entreeNumberLabel.AutoSize = true;
+            this.entreeNumberLabel.Location = new System.Drawing.Point(150, 194);
+            this.entreeNumberLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.entreeNumberLabel.Name = "entreeNumberLabel";
+            this.entreeNumberLabel.Size = new System.Drawing.Size(208, 25);
+            this.entreeNumberLabel.TabIndex = 1;
+            this.entreeNumberLabel.Text = "Entree/Item Number:";
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(164, 271);
+            this.quantityLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(202, 25);
+            this.quantityLabel.TabIndex = 2;
+            this.quantityLabel.Text = "Quantity (Servings):";
+            // 
+            // initialCostLabel
+            // 
+            this.initialCostLabel.AutoSize = true;
+            this.initialCostLabel.Location = new System.Drawing.Point(246, 408);
+            this.initialCostLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.initialCostLabel.Name = "initialCostLabel";
+            this.initialCostLabel.Size = new System.Drawing.Size(118, 25);
+            this.initialCostLabel.TabIndex = 3;
+            this.initialCostLabel.Text = "Initial Cost:";
+            // 
+            // adjustedCostLabel
+            // 
+            this.adjustedCostLabel.AutoSize = true;
+            this.adjustedCostLabel.Location = new System.Drawing.Point(212, 479);
+            this.adjustedCostLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.adjustedCostLabel.Name = "adjustedCostLabel";
+            this.adjustedCostLabel.Size = new System.Drawing.Size(152, 25);
+            this.adjustedCostLabel.TabIndex = 4;
+            this.adjustedCostLabel.Text = "Adjusted Cost:";
+            // 
+            // shipmentCostLabel
+            // 
+            this.shipmentCostLabel.AutoSize = true;
+            this.shipmentCostLabel.Location = new System.Drawing.Point(206, 548);
+            this.shipmentCostLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.shipmentCostLabel.Name = "shipmentCostLabel";
+            this.shipmentCostLabel.Size = new System.Drawing.Size(158, 25);
+            this.shipmentCostLabel.TabIndex = 5;
+            this.shipmentCostLabel.Text = "Shipment Cost:";
+            // 
+            // totalPriceLabel
+            // 
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Location = new System.Drawing.Point(240, 621);
+            this.totalPriceLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(121, 25);
+            this.totalPriceLabel.TabIndex = 6;
+            this.totalPriceLabel.Text = "Total Price:";
+            // 
+            // cityComboBox
+            // 
+            this.cityComboBox.BackColor = System.Drawing.SystemColors.Control;
+            this.cityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cityComboBox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cityComboBox.FormattingEnabled = true;
+            this.cityComboBox.Location = new System.Drawing.Point(376, 117);
+            this.cityComboBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.cityComboBox.Name = "cityComboBox";
+            this.cityComboBox.Size = new System.Drawing.Size(238, 33);
+            this.cityComboBox.TabIndex = 7;
+            // 
+            // entreeNumberTextBox
+            // 
+            this.entreeNumberTextBox.Location = new System.Drawing.Point(376, 194);
+            this.entreeNumberTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.entreeNumberTextBox.Name = "entreeNumberTextBox";
+            this.entreeNumberTextBox.Size = new System.Drawing.Size(238, 31);
+            this.entreeNumberTextBox.TabIndex = 8;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(376, 271);
+            this.quantityTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(238, 31);
+            this.quantityTextBox.TabIndex = 9;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(426, 338);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(150, 44);
+            this.calculateButton.TabIndex = 10;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // initialCostOutputLabel
+            // 
+            this.initialCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.initialCostOutputLabel.Location = new System.Drawing.Point(376, 408);
+            this.initialCostOutputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.initialCostOutputLabel.Name = "initialCostOutputLabel";
+            this.initialCostOutputLabel.Size = new System.Drawing.Size(240, 37);
+            this.initialCostOutputLabel.TabIndex = 11;
+            // 
+            // adjustedCostOutputLabel
+            // 
+            this.adjustedCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.adjustedCostOutputLabel.Location = new System.Drawing.Point(376, 479);
+            this.adjustedCostOutputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.adjustedCostOutputLabel.Name = "adjustedCostOutputLabel";
+            this.adjustedCostOutputLabel.Size = new System.Drawing.Size(240, 37);
+            this.adjustedCostOutputLabel.TabIndex = 12;
+            // 
+            // shipmentCostOutputLabel
+            // 
+            this.shipmentCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.shipmentCostOutputLabel.Location = new System.Drawing.Point(376, 548);
+            this.shipmentCostOutputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.shipmentCostOutputLabel.Name = "shipmentCostOutputLabel";
+            this.shipmentCostOutputLabel.Size = new System.Drawing.Size(240, 37);
+            this.shipmentCostOutputLabel.TabIndex = 13;
+            // 
+            // totalPriceOutputLabel
+            // 
+            this.totalPriceOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalPriceOutputLabel.Location = new System.Drawing.Point(376, 621);
+            this.totalPriceOutputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.totalPriceOutputLabel.Name = "totalPriceOutputLabel";
+            this.totalPriceOutputLabel.Size = new System.Drawing.Size(240, 37);
+            this.totalPriceOutputLabel.TabIndex = 14;
+            // 
+            // Prog3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 865);
+            this.Controls.Add(this.totalPriceOutputLabel);
+            this.Controls.Add(this.shipmentCostOutputLabel);
+            this.Controls.Add(this.adjustedCostOutputLabel);
+            this.Controls.Add(this.initialCostOutputLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.entreeNumberTextBox);
+            this.Controls.Add(this.cityComboBox);
+            this.Controls.Add(this.totalPriceLabel);
+            this.Controls.Add(this.shipmentCostLabel);
+            this.Controls.Add(this.adjustedCostLabel);
+            this.Controls.Add(this.initialCostLabel);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.entreeNumberLabel);
+            this.Controls.Add(this.cityLabel);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Prog3";
+            this.Text = "uThankItWeMakeIt Cost Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label entreeNumberLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Label initialCostLabel;
+        private System.Windows.Forms.Label adjustedCostLabel;
+        private System.Windows.Forms.Label shipmentCostLabel;
+        private System.Windows.Forms.Label totalPriceLabel;
+        private System.Windows.Forms.ComboBox cityComboBox;
+        private System.Windows.Forms.TextBox entreeNumberTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label initialCostOutputLabel;
+        private System.Windows.Forms.Label adjustedCostOutputLabel;
+        private System.Windows.Forms.Label shipmentCostOutputLabel;
+        private System.Windows.Forms.Label totalPriceOutputLabel;
+    }
+}
+
---
+++ Prog3/Prog3/Prog3.cs
@@ -0,0 +1,97 @@
+﻿//Grading ID: K6509
+//CIS 199-01
+//Due Date: 11/11/2021
+//This program displays a form to the user, which asks for a selection of a city to ship to,
+//the entree item number, and the number of servings/quantity requested.
+//The program then outputs 4 costs to the user, the initial cost, adjusted cost, shipping total,
+//and total cost overall. This program primarily uses for loops and arrays to search for corresponding data.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog3
+{
+    public partial class Prog3 : Form
+    {
+        public Prog3()
+        {
+            InitializeComponent();
+            //comboBox item declaration
+            cityComboBox.Items.Add("Louisville"); //index 0
+            cityComboBox.Items.Add("Lexington"); //index 1
+            cityComboBox.Items.Add("Indianapolis"); //index 2
+            cityComboBox.Items.Add("Nashville"); //index 3
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //declaring arrays for stored values
+            double[] shipments = { 0.06, 0.0717, 0.07, 0.0874 }; //shipment fee percentages
+            int[] entrees = { 10001, 10002, 10003, 10004, 10005, 10006, 10007 }; //entree ID numbers
+            double[] costs = { 7.87, 9.51, 10.73, 9.99, 11.99, 5.00, 4.58 }; //serving costs
+            int[] servings = { 21, 11, 6, 1 }; //number of servings, ranges
+            double[] fees = { 0, .05, .10, .15 }; //service fees
+
+            //declaring variables for calculations
+            int entreeNumberInput = Convert.ToInt32(entreeNumberTextBox.Text); //convert entree number to integer
+            int quantityInput = Convert.ToInt32(quantityTextBox.Text); //convert serving quantity to integer
+            double shipmentCost = 0; //initialize shipping cost
+            double shippingTotal = 0; //intialize total shipping cost
+            double servingCost = 0; //initialize serving cost
+            double serviceFee = 0; //initialize service fee
+            double totalCost = 0; //initialize total cost
+            double initialCost = 0; //initialize initial cost
+            double adjustedCost = 0; //initialize adjusted cost
+            bool found = false; //declaring boolean value for the for loops
+
+            if (cityComboBox.SelectedIndex > -1) //checking if user selected a city from drop down
+            {
+                shipmentCost = shipments[cityComboBox.SelectedIndex]; //setting shipment cost accordingly from shipments array
+            }
+
+            for(int i = 0; i <= entrees.Length-1 && !found; i++) //for loop to check entree number
+            {
+                if(entreeNumberInput == entrees[i]) //checking if user input matches a value in the entrees array
+                {
+                    servingCost = costs[i]; //setting serving cost accordingly from servings cost array
+                    found = true; //flagging boolean value to exit the loop
+                }
+            }
+
+            found = false; //reset the boolean value for next for loop
+            
+            for (int i = 0; i <= servings.Length - 1 && !found; i++) //for loop to check quantity/servings input
+            {
+                if(quantityInput >= servings[i]) //checking if user input matches a range in the servings array
+                {
+                    serviceFee = fees[i]; //setting service fee accoringly from fees array
+                    found = true; //flagging boolean value to exit the loop
+                }
+            }
+            
+            if(servingCost > 0 && quantityInput > 0 && shipmentCost > 0) //validating user inputs
+            {
+                initialCost = servingCost * quantityInput; //calculating initial cost
+                adjustedCost = (initialCost * serviceFee) + initialCost; //calculating adjusted cost
+                shippingTotal = shipmentCost * adjustedCost; //calculating the total shipping cost
+                totalCost = adjustedCost + shippingTotal; //calculating total value
+            }
+            else //else statement if user input is invalid
+            {
+                MessageBox.Show("Invalid input."); //Message Box to user to indicate an error
+            }
+
+            //Displaying values out to the user
+            initialCostOutputLabel.Text = initialCost.ToString($"C"); //Outputting initial cost to the label
+            adjustedCostOutputLabel.Text = adjustedCost.ToString($"C"); //Outputting adjusted cost to the label
+            shipmentCostOutputLabel.Text = shippingTotal.ToString($"C"); //Outputting shipping total to the label
+            totalPriceOutputLabel.Text = totalCost.ToString($"C"); //Outputting total cost to the label
+        }
+    }
+}
