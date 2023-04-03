4deeef5df64ad5fb7982e8820a809d24c410a6c2
d-hutchins <129690658+d-hutchins@users.noreply.github.com>
2023-04-02 20:58:57 -0400

    Add files via upload

---
+++ Program2/Program2/Program2.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace Program2
+{
+    partial class Program2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passengerInputTextBox = new System.Windows.Forms.TextBox();
+            this.passengerLabel = new System.Windows.Forms.Label();
+            this.distanceLabel = new System.Windows.Forms.Label();
+            this.distanceInputTextBox = new System.Windows.Forms.TextBox();
+            this.carTypeLabel = new System.Windows.Forms.Label();
+            this.carTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.calculateCostButton = new System.Windows.Forms.Button();
+            this.resultsGroupBox = new System.Windows.Forms.GroupBox();
+            this.lowestCostOutputLabel = new System.Windows.Forms.Label();
+            this.thirdCompanyOutputLabel = new System.Windows.Forms.Label();
+            this.secondCompanyOutputLabel = new System.Windows.Forms.Label();
+            this.firstCompanyOutputLabel = new System.Windows.Forms.Label();
+            this.thirdCompanyTitleLabel = new System.Windows.Forms.Label();
+            this.secondCompanyTitleLabel = new System.Windows.Forms.Label();
+            this.firstCompanyTitleLabel = new System.Windows.Forms.Label();
+            this.resultsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // passengerInputTextBox
+            // 
+            this.passengerInputTextBox.Location = new System.Drawing.Point(275, 146);
+            this.passengerInputTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.passengerInputTextBox.Name = "passengerInputTextBox";
+            this.passengerInputTextBox.Size = new System.Drawing.Size(196, 31);
+            this.passengerInputTextBox.TabIndex = 0;
+            // 
+            // passengerLabel
+            // 
+            this.passengerLabel.AutoSize = true;
+            this.passengerLabel.Location = new System.Drawing.Point(64, 146);
+            this.passengerLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.passengerLabel.Name = "passengerLabel";
+            this.passengerLabel.Size = new System.Drawing.Size(189, 25);
+            this.passengerLabel.TabIndex = 1;
+            this.passengerLabel.Text = "Passengers (1-12)";
+            // 
+            // distanceLabel
+            // 
+            this.distanceLabel.AutoSize = true;
+            this.distanceLabel.Location = new System.Drawing.Point(86, 232);
+            this.distanceLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.distanceLabel.Name = "distanceLabel";
+            this.distanceLabel.Size = new System.Drawing.Size(167, 25);
+            this.distanceLabel.TabIndex = 2;
+            this.distanceLabel.Text = "Distance (Miles)";
+            // 
+            // distanceInputTextBox
+            // 
+            this.distanceInputTextBox.Location = new System.Drawing.Point(275, 232);
+            this.distanceInputTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.distanceInputTextBox.Name = "distanceInputTextBox";
+            this.distanceInputTextBox.Size = new System.Drawing.Size(196, 31);
+            this.distanceInputTextBox.TabIndex = 3;
+            // 
+            // carTypeLabel
+            // 
+            this.carTypeLabel.AutoSize = true;
+            this.carTypeLabel.Location = new System.Drawing.Point(147, 315);
+            this.carTypeLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.carTypeLabel.Name = "carTypeLabel";
+            this.carTypeLabel.Size = new System.Drawing.Size(106, 25);
+            this.carTypeLabel.TabIndex = 4;
+            this.carTypeLabel.Text = "Car Type:";
+            // 
+            // carTypeComboBox
+            // 
+            this.carTypeComboBox.Location = new System.Drawing.Point(275, 315);
+            this.carTypeComboBox.Margin = new System.Windows.Forms.Padding(6);
+            this.carTypeComboBox.Name = "carTypeComboBox";
+            this.carTypeComboBox.Size = new System.Drawing.Size(238, 33);
+            this.carTypeComboBox.TabIndex = 9;
+            // 
+            // calculateCostButton
+            // 
+            this.calculateCostButton.Location = new System.Drawing.Point(173, 403);
+            this.calculateCostButton.Margin = new System.Windows.Forms.Padding(6);
+            this.calculateCostButton.Name = "calculateCostButton";
+            this.calculateCostButton.Size = new System.Drawing.Size(212, 44);
+            this.calculateCostButton.TabIndex = 6;
+            this.calculateCostButton.Text = "Calculate Cost";
+            this.calculateCostButton.UseVisualStyleBackColor = true;
+            this.calculateCostButton.Click += new System.EventHandler(this.CalculateCostButton_Click);
+            // 
+            // resultsGroupBox
+            // 
+            this.resultsGroupBox.Controls.Add(this.lowestCostOutputLabel);
+            this.resultsGroupBox.Controls.Add(this.thirdCompanyOutputLabel);
+            this.resultsGroupBox.Controls.Add(this.secondCompanyOutputLabel);
+            this.resultsGroupBox.Controls.Add(this.firstCompanyOutputLabel);
+            this.resultsGroupBox.Controls.Add(this.thirdCompanyTitleLabel);
+            this.resultsGroupBox.Controls.Add(this.secondCompanyTitleLabel);
+            this.resultsGroupBox.Controls.Add(this.firstCompanyTitleLabel);
+            this.resultsGroupBox.Location = new System.Drawing.Point(542, 49);
+            this.resultsGroupBox.Margin = new System.Windows.Forms.Padding(6);
+            this.resultsGroupBox.Name = "resultsGroupBox";
+            this.resultsGroupBox.Padding = new System.Windows.Forms.Padding(6);
+            this.resultsGroupBox.Size = new System.Drawing.Size(573, 552);
+            this.resultsGroupBox.TabIndex = 8;
+            this.resultsGroupBox.TabStop = false;
+            this.resultsGroupBox.Text = "Results";
+            // 
+            // lowestCostOutputLabel
+            // 
+            this.lowestCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lowestCostOutputLabel.Location = new System.Drawing.Point(53, 425);
+            this.lowestCostOutputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lowestCostOutputLabel.Name = "lowestCostOutputLabel";
+            this.lowestCostOutputLabel.Size = new System.Drawing.Size(481, 42);
+            this.lowestCostOutputLabel.TabIndex = 6;
+            this.lowestCostOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // thirdCompanyOutputLabel
+            // 
+            this.thirdCompanyOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.thirdCompanyOutputLabel.Location = new System.Drawing.Point(336, 318);
+            this.thirdCompanyOutputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.thirdCompanyOutputLabel.Name = "thirdCompanyOutputLabel";
+            this.thirdCompanyOutputLabel.Size = new System.Drawing.Size(198, 42);
+            this.thirdCompanyOutputLabel.TabIndex = 5;
+            this.thirdCompanyOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // secondCompanyOutputLabel
+            // 
+            this.secondCompanyOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.secondCompanyOutputLabel.Location = new System.Drawing.Point(336, 210);
+            this.secondCompanyOutputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.secondCompanyOutputLabel.Name = "secondCompanyOutputLabel";
+            this.secondCompanyOutputLabel.Size = new System.Drawing.Size(198, 42);
+            this.secondCompanyOutputLabel.TabIndex = 4;
+            this.secondCompanyOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // firstCompanyOutputLabel
+            // 
+            this.firstCompanyOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.firstCompanyOutputLabel.Location = new System.Drawing.Point(336, 102);
+            this.firstCompanyOutputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.firstCompanyOutputLabel.Name = "firstCompanyOutputLabel";
+            this.firstCompanyOutputLabel.Size = new System.Drawing.Size(198, 42);
+            this.firstCompanyOutputLabel.TabIndex = 3;
+            this.firstCompanyOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // thirdCompanyTitleLabel
+            // 
+            this.thirdCompanyTitleLabel.AutoSize = true;
+            this.thirdCompanyTitleLabel.Location = new System.Drawing.Point(112, 318);
+            this.thirdCompanyTitleLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.thirdCompanyTitleLabel.Name = "thirdCompanyTitleLabel";
+            this.thirdCompanyTitleLabel.Size = new System.Drawing.Size(174, 25);
+            this.thirdCompanyTitleLabel.TabIndex = 2;
+            this.thirdCompanyTitleLabel.Text = "Company C Cost";
+            // 
+            // secondCompanyTitleLabel
+            // 
+            this.secondCompanyTitleLabel.AutoSize = true;
+            this.secondCompanyTitleLabel.Location = new System.Drawing.Point(112, 210);
+            this.secondCompanyTitleLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.secondCompanyTitleLabel.Name = "secondCompanyTitleLabel";
+            this.secondCompanyTitleLabel.Size = new System.Drawing.Size(173, 25);
+            this.secondCompanyTitleLabel.TabIndex = 1;
+            this.secondCompanyTitleLabel.Text = "Company B Cost";
+            // 
+            // firstCompanyTitleLabel
+            // 
+            this.firstCompanyTitleLabel.AutoSize = true;
+            this.firstCompanyTitleLabel.Location = new System.Drawing.Point(112, 102);
+            this.firstCompanyTitleLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.firstCompanyTitleLabel.Name = "firstCompanyTitleLabel";
+            this.firstCompanyTitleLabel.Size = new System.Drawing.Size(173, 25);
+            this.firstCompanyTitleLabel.TabIndex = 0;
+            this.firstCompanyTitleLabel.Text = "Company A Cost";
+            // 
+            // Program2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.resultsGroupBox);
+            this.Controls.Add(this.calculateCostButton);
+            this.Controls.Add(this.carTypeComboBox);
+            this.Controls.Add(this.carTypeLabel);
+            this.Controls.Add(this.distanceInputTextBox);
+            this.Controls.Add(this.distanceLabel);
+            this.Controls.Add(this.passengerLabel);
+            this.Controls.Add(this.passengerInputTextBox);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Program2";
+            this.Text = "iRydeShare Cost Calculation";
+            this.resultsGroupBox.ResumeLayout(false);
+            this.resultsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox passengerInputTextBox;
+        private System.Windows.Forms.Label passengerLabel;
+        private System.Windows.Forms.Label distanceLabel;
+        private System.Windows.Forms.TextBox distanceInputTextBox;
+        private System.Windows.Forms.Label carTypeLabel;
+        private System.Windows.Forms.ComboBox carTypeComboBox;
+        private System.Windows.Forms.Button calculateCostButton;
+        private System.Windows.Forms.GroupBox resultsGroupBox;
+        private System.Windows.Forms.Label lowestCostOutputLabel;
+        private System.Windows.Forms.Label thirdCompanyOutputLabel;
+        private System.Windows.Forms.Label secondCompanyOutputLabel;
+        private System.Windows.Forms.Label firstCompanyOutputLabel;
+        private System.Windows.Forms.Label thirdCompanyTitleLabel;
+        private System.Windows.Forms.Label secondCompanyTitleLabel;
+        private System.Windows.Forms.Label firstCompanyTitleLabel;
+    }
+}
+
---
+++ Program2/Program2/Program2.cs
@@ -0,0 +1,195 @@
+﻿//Program 2
+//Grading ID: K6509
+//CIS 199-01
+//Due Date: 10/21/2021
+//This program prompts for the user to input a number of passengers and mileage for a ride share rental.
+//It also utilizes a combo box to give the user options on what tier of ride they want.
+//Then the program calculates three different costs for three companies, outputs them to the user.
+//It has a final message showing which company has the lowest cost.
+//If the user inputs invalid data, there are multiple validation messages that are displayed.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Program2 : Form
+    {
+        public Program2()
+        {
+            InitializeComponent();
+            //Creating list of Combo Box Items
+            carTypeComboBox.Items.Add("Limousine"); //index 0
+            carTypeComboBox.Items.Add("Luxury"); //index 1
+            carTypeComboBox.Items.Add("Mid-Tier"); //index 2
+            carTypeComboBox.Items.Add("Green"); //index 3
+            carTypeComboBox.Items.Add("Economy"); //index 4
+        }
+
+        //event handler for calculate button
+        private void CalculateCostButton_Click(object sender, EventArgs e)
+        {
+            //initializing variables for calculations
+            int passengerInput = 0; //integer value for passenger count
+            double distanceInput = 0; //double value for distance input
+            double companyOneTotalCost = 0; //double Company A total calculated cost
+            double companyTwoTotalCost = 0; //double Company B total calculated cost
+            double companyThreeTotalCost = 0; //double Company C total calculated cost
+
+            int passengerCostOne = 0; //integer value for passenger cost of Company A
+            int passengerCostTwo = 0; //integer value for passenger cost of Company B
+            double passengerCostThree = 0; //double  value for passenger cost of Company C
+
+            double mileageOneCost = 0; //double value for mileage cost of Company A
+            double mileageTwoCost = 0; //double value for mileage cost of Company B
+            double mileageThreeCost = 0; //double value for mileage cost of Company C
+
+            int carTypeOne = 0; //integer value for cost of selected car type with Company A
+            int carTypeTwo = 0; //integer value for cost of selected car type with Company B
+            const int carTypeThree = 20; //constant integer value for selected car type with Company C
+
+            //main if statement for entire function of program
+            if (int.TryParse(passengerInputTextBox.Text, out passengerInput) && double.TryParse(distanceInputTextBox.Text, out distanceInput) && passengerInput < 13 && passengerInput > 0 && distanceInput > 0)
+            {
+                passengerCostOne = 2 * passengerInput; //calculating Company A passenger cost
+                passengerCostThree = .25 * passengerInput; //calculating Company C passenger cost
+
+                mileageOneCost = .02 * distanceInput; //calculating Company A distance cost
+                mileageTwoCost = .10 * distanceInput; //calculating Company B distance cost
+
+                //nested if, else if statements for passenger count checks
+                if (passengerInput <= 2) //passenger count 2 or less
+                {
+                    passengerCostTwo = 20 * passengerInput; //calculate Company B passenger cost
+                }
+                else if (passengerInput >= 3 && passengerInput <= 6) //passenger count between 3 and 6
+                {
+                    passengerCostTwo = 15 * passengerInput; //calculate Company B passenger cost
+                }
+                else //passenger count between 7 and 12
+                {
+                    passengerCostTwo = 5 * passengerInput; //calculate Company B passenger cost
+                }
+                
+                //nested if, else if statements for distance input checks
+                if (distanceInput > 200) //distance greater than 200
+                {
+                    mileageThreeCost = 40; //setting mileage cost for Company C
+                }
+                else if (distanceInput >= 100 && distanceInput <= 199) //distance between 100 and 199
+                {
+                    mileageThreeCost = 35; //setting mileage cost for Company C
+                }
+                else if (distanceInput >= 50 && distanceInput <= 99) //distance between 50 and 99
+                {
+                    mileageThreeCost = 25; //setting mileage cost for Company C
+                }
+                else if (distanceInput >= 10 && distanceInput <= 49) //distance between 10 and 49
+                {
+                    mileageThreeCost = 15; //setting mileage cost for Company C
+                }
+                else //distance between 1 and 9
+                {
+                    mileageThreeCost = 5; //setting mileage cost for Company C
+                }
+
+                //switch cases depending on selected car type in combo box
+                switch (carTypeComboBox.SelectedIndex)
+                {
+                    //Limosuine selected
+                    case 0:
+                        carTypeOne = 50; //setting car cost for Company A
+                        carTypeTwo = 40; //setting car cost for Company B
+                        break;
+
+                    //Luxury selected
+                    case 1:
+                        carTypeOne = 40; //setting car cost for Company A
+                        carTypeTwo = 40; //setting car cost for Company B
+                        break;
+
+                    //Mid-Tier selected
+                    case 2:
+                        carTypeOne = 25; //setting car cost for Company A
+                        carTypeTwo = 25; //setting car cost for Company B
+                        break;
+
+                    //Green selected
+                    case 3:
+                        carTypeOne = 15; //setting car cost for Company A
+                        carTypeTwo = 15; //setting car cost for Company B
+                        break;
+
+                    //Economy selected
+                    case 4:
+                        carTypeOne = 7; //setting car cost for Company A
+                        carTypeTwo = 15; //setting car cost for Company B
+                        break;
+
+                    //if user types into combo box or does not select a car type
+                    default:
+                        MessageBox.Show("Car type not valid."); //Error message to user
+                        break;
+                }
+                //Calculating total costs for the three companies
+                companyOneTotalCost = carTypeOne + mileageOneCost + passengerCostOne; //total cost calculation for Company A
+                companyTwoTotalCost = carTypeTwo + mileageTwoCost + passengerCostTwo; //total cost calculation for Company B
+                companyThreeTotalCost = carTypeThree + mileageThreeCost + passengerCostThree; //total cost calculation for Company C
+                
+                //Outputting calculations to labels in form with currency formatting
+                firstCompanyOutputLabel.Text = companyOneTotalCost.ToString($"C"); //outputting total for Company A
+                secondCompanyOutputLabel.Text = companyTwoTotalCost.ToString($"C"); //outputting total for Company B
+                thirdCompanyOutputLabel.Text = companyThreeTotalCost.ToString($"C"); //outputting total for Company C
+                
+                //validating if totals are lower than one another or equivalent to each other
+                if (companyOneTotalCost < companyTwoTotalCost && companyOneTotalCost < companyThreeTotalCost) //Company A lowest cost
+                {
+                    lowestCostOutputLabel.Text = $"Lowest cost company is: A."; //output message in bottom label for Company A
+                }
+                else if (companyTwoTotalCost < companyOneTotalCost && companyTwoTotalCost < companyThreeTotalCost) //Company B lowest cost
+                {
+                    lowestCostOutputLabel.Text = $"Lowest cost company is: B."; //output message in bottom label for Company B
+                }
+                else if (companyThreeTotalCost < companyOneTotalCost && companyThreeTotalCost < companyTwoTotalCost) //Company C lowest cost
+                {
+                    lowestCostOutputLabel.Text = $"Lowest cost company is: C."; //output message in bottom label for Company C
+                }
+                else if (companyOneTotalCost == companyTwoTotalCost) //checking for tie between Companies A and B
+                {
+                    lowestCostOutputLabel.Text = $"There is a tie between companies A and B."; //output message to bottom label for tie with A and B
+                }
+                else if (companyOneTotalCost == companyThreeTotalCost) //checking for tie between Companies A and C
+                {
+                    lowestCostOutputLabel.Text = $"There is a tie between companies A and C."; //output message in bottom label for tie between A and C
+                }
+                else if (companyTwoTotalCost == companyThreeTotalCost) //checking for tie between Companies B and C
+                {
+                    lowestCostOutputLabel.Text = $"There is a tie between companies B and C."; //output message to bottom label for tie between B and C
+                }
+            }
+            else if(!int.TryParse(passengerInputTextBox.Text, out passengerInput)) //checking for invalid passenger input
+            {
+                MessageBox.Show("Invalid input. Please enter an integer value between 1 and 12."); //error message to user
+            }
+            else if(passengerInput > 12) //checking if passenger count is greater than 12
+            {
+                MessageBox.Show("Total passengers cannot be greater than 12."); //error message to user
+            }
+            else if (passengerInput < 1) //checking if passenger count is less than 1 (negative)
+            {
+                MessageBox.Show("Total passengers must be greater than 0."); //error message to user
+            }
+            else if(!double.TryParse(distanceInputTextBox.Text, out distanceInput) || distanceInput < 1) //checking if distance input is invalid or less than 1 (negative)
+            {
+                MessageBox.Show("Invalid input. Please enter a distance value greater than 1 mile."); //error message to user
+            }
+        }
+    }
+}
