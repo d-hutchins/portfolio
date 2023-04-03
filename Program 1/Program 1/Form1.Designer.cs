209730a0423d90fca3ee125b7e0d2a1e448ffa49
d-hutchins <129690658+d-hutchins@users.noreply.github.com>
2023-04-02 20:57:24 -0400

    Add files via upload

---
+++ Program 1/Program 1/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿
+namespace Program_1
+{
+    partial class Program1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.maxWidthTitleLabel = new System.Windows.Forms.Label();
+            this.maxLengthTitleLabel = new System.Windows.Forms.Label();
+            this.maxDepthTitleLabel = new System.Windows.Forms.Label();
+            this.materialPriceTitleLabel = new System.Windows.Forms.Label();
+            this.excavationTitleLabel = new System.Windows.Forms.Label();
+            this.excavationNoteLabel = new System.Windows.Forms.Label();
+            this.divingBoardTitleLabel = new System.Windows.Forms.Label();
+            this.divingNoteLabel = new System.Windows.Forms.Label();
+            this.cubicYardsTitleLabel = new System.Windows.Forms.Label();
+            this.cubicYardsOutputLabel = new System.Windows.Forms.Label();
+            this.materialCostTitleLabel = new System.Windows.Forms.Label();
+            this.materialCostOutputLabel = new System.Windows.Forms.Label();
+            this.excavationCostTitleLabel = new System.Windows.Forms.Label();
+            this.excavationCostOutputLabel = new System.Windows.Forms.Label();
+            this.laborCostTitleLabel = new System.Windows.Forms.Label();
+            this.laborCostOutputLabel = new System.Windows.Forms.Label();
+            this.totalCostTitleLabel = new System.Windows.Forms.Label();
+            this.totalCostOutputLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.userWidthTextBox = new System.Windows.Forms.TextBox();
+            this.userLengthTextBox = new System.Windows.Forms.TextBox();
+            this.userDepthTextBox = new System.Windows.Forms.TextBox();
+            this.userMaterialPriceTextBox = new System.Windows.Forms.TextBox();
+            this.excavationTextBox = new System.Windows.Forms.TextBox();
+            this.divingBoardTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(124, 18);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(125, 13);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "EZ-Pools Costs Estimator";
+            // 
+            // maxWidthTitleLabel
+            // 
+            this.maxWidthTitleLabel.AutoSize = true;
+            this.maxWidthTitleLabel.Location = new System.Drawing.Point(70, 57);
+            this.maxWidthTitleLabel.Name = "maxWidthTitleLabel";
+            this.maxWidthTitleLabel.Size = new System.Drawing.Size(112, 13);
+            this.maxWidthTitleLabel.TabIndex = 1;
+            this.maxWidthTitleLabel.Text = "Max Width of Pool (ft):";
+            // 
+            // maxLengthTitleLabel
+            // 
+            this.maxLengthTitleLabel.AutoSize = true;
+            this.maxLengthTitleLabel.Location = new System.Drawing.Point(65, 89);
+            this.maxLengthTitleLabel.Name = "maxLengthTitleLabel";
+            this.maxLengthTitleLabel.Size = new System.Drawing.Size(117, 13);
+            this.maxLengthTitleLabel.TabIndex = 3;
+            this.maxLengthTitleLabel.Text = "Max Length of Pool (ft):";
+            // 
+            // maxDepthTitleLabel
+            // 
+            this.maxDepthTitleLabel.AutoSize = true;
+            this.maxDepthTitleLabel.Location = new System.Drawing.Point(69, 122);
+            this.maxDepthTitleLabel.Name = "maxDepthTitleLabel";
+            this.maxDepthTitleLabel.Size = new System.Drawing.Size(113, 13);
+            this.maxDepthTitleLabel.TabIndex = 5;
+            this.maxDepthTitleLabel.Text = "Max Depth of Pool (ft):";
+            // 
+            // materialPriceTitleLabel
+            // 
+            this.materialPriceTitleLabel.AutoSize = true;
+            this.materialPriceTitleLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.materialPriceTitleLabel.Location = new System.Drawing.Point(54, 158);
+            this.materialPriceTitleLabel.Name = "materialPriceTitleLabel";
+            this.materialPriceTitleLabel.Size = new System.Drawing.Size(128, 13);
+            this.materialPriceTitleLabel.TabIndex = 7;
+            this.materialPriceTitleLabel.Text = "Materials Price (cubic yd):";
+            // 
+            // excavationTitleLabel
+            // 
+            this.excavationTitleLabel.AutoSize = true;
+            this.excavationTitleLabel.Location = new System.Drawing.Point(72, 189);
+            this.excavationTitleLabel.Name = "excavationTitleLabel";
+            this.excavationTitleLabel.Size = new System.Drawing.Size(110, 13);
+            this.excavationTitleLabel.TabIndex = 9;
+            this.excavationTitleLabel.Text = "Excavation Needed?:";
+            // 
+            // excavationNoteLabel
+            // 
+            this.excavationNoteLabel.AutoSize = true;
+            this.excavationNoteLabel.Location = new System.Drawing.Point(81, 202);
+            this.excavationNoteLabel.Name = "excavationNoteLabel";
+            this.excavationNoteLabel.Size = new System.Drawing.Size(92, 13);
+            this.excavationNoteLabel.TabIndex = 11;
+            this.excavationNoteLabel.Text = "(1 = YES, 0 = NO)";
+            // 
+            // divingBoardTitleLabel
+            // 
+            this.divingBoardTitleLabel.AutoSize = true;
+            this.divingBoardTitleLabel.Location = new System.Drawing.Point(105, 233);
+            this.divingBoardTitleLabel.Name = "divingBoardTitleLabel";
+            this.divingBoardTitleLabel.Size = new System.Drawing.Size(77, 13);
+            this.divingBoardTitleLabel.TabIndex = 12;
+            this.divingBoardTitleLabel.Text = "Diving Board?:";
+            // 
+            // divingNoteLabel
+            // 
+            this.divingNoteLabel.AutoSize = true;
+            this.divingNoteLabel.Location = new System.Drawing.Point(90, 255);
+            this.divingNoteLabel.Name = "divingNoteLabel";
+            this.divingNoteLabel.Size = new System.Drawing.Size(92, 13);
+            this.divingNoteLabel.TabIndex = 14;
+            this.divingNoteLabel.Text = "(1 = YES, 0 = NO)";
+            // 
+            // cubicYardsTitleLabel
+            // 
+            this.cubicYardsTitleLabel.AutoSize = true;
+            this.cubicYardsTitleLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.cubicYardsTitleLabel.Location = new System.Drawing.Point(115, 320);
+            this.cubicYardsTitleLabel.Name = "cubicYardsTitleLabel";
+            this.cubicYardsTitleLabel.Size = new System.Drawing.Size(67, 13);
+            this.cubicYardsTitleLabel.TabIndex = 15;
+            this.cubicYardsTitleLabel.Text = "Cubic Yards:";
+            // 
+            // cubicYardsOutputLabel
+            // 
+            this.cubicYardsOutputLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.cubicYardsOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.cubicYardsOutputLabel.Location = new System.Drawing.Point(188, 320);
+            this.cubicYardsOutputLabel.Name = "cubicYardsOutputLabel";
+            this.cubicYardsOutputLabel.Size = new System.Drawing.Size(100, 15);
+            this.cubicYardsOutputLabel.TabIndex = 16;
+            // 
+            // materialCostTitleLabel
+            // 
+            this.materialCostTitleLabel.AutoSize = true;
+            this.materialCostTitleLabel.Location = new System.Drawing.Point(106, 357);
+            this.materialCostTitleLabel.Name = "materialCostTitleLabel";
+            this.materialCostTitleLabel.Size = new System.Drawing.Size(76, 13);
+            this.materialCostTitleLabel.TabIndex = 17;
+            this.materialCostTitleLabel.Text = "Materials Cost:";
+            // 
+            // materialCostOutputLabel
+            // 
+            this.materialCostOutputLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.materialCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.materialCostOutputLabel.Location = new System.Drawing.Point(188, 356);
+            this.materialCostOutputLabel.Name = "materialCostOutputLabel";
+            this.materialCostOutputLabel.Size = new System.Drawing.Size(100, 15);
+            this.materialCostOutputLabel.TabIndex = 18;
+            // 
+            // excavationCostTitleLabel
+            // 
+            this.excavationCostTitleLabel.AutoSize = true;
+            this.excavationCostTitleLabel.Location = new System.Drawing.Point(95, 391);
+            this.excavationCostTitleLabel.Name = "excavationCostTitleLabel";
+            this.excavationCostTitleLabel.Size = new System.Drawing.Size(87, 13);
+            this.excavationCostTitleLabel.TabIndex = 19;
+            this.excavationCostTitleLabel.Text = "Excavation Cost:";
+            // 
+            // excavationCostOutputLabel
+            // 
+            this.excavationCostOutputLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.excavationCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.excavationCostOutputLabel.Location = new System.Drawing.Point(188, 391);
+            this.excavationCostOutputLabel.Name = "excavationCostOutputLabel";
+            this.excavationCostOutputLabel.Size = new System.Drawing.Size(100, 15);
+            this.excavationCostOutputLabel.TabIndex = 20;
+            // 
+            // laborCostTitleLabel
+            // 
+            this.laborCostTitleLabel.AutoSize = true;
+            this.laborCostTitleLabel.Location = new System.Drawing.Point(121, 426);
+            this.laborCostTitleLabel.Name = "laborCostTitleLabel";
+            this.laborCostTitleLabel.Size = new System.Drawing.Size(61, 13);
+            this.laborCostTitleLabel.TabIndex = 21;
+            this.laborCostTitleLabel.Text = "Labor Cost:";
+            // 
+            // laborCostOutputLabel
+            // 
+            this.laborCostOutputLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.laborCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.laborCostOutputLabel.Location = new System.Drawing.Point(188, 424);
+            this.laborCostOutputLabel.Name = "laborCostOutputLabel";
+            this.laborCostOutputLabel.Size = new System.Drawing.Size(100, 15);
+            this.laborCostOutputLabel.TabIndex = 22;
+            // 
+            // totalCostTitleLabel
+            // 
+            this.totalCostTitleLabel.AutoSize = true;
+            this.totalCostTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalCostTitleLabel.Location = new System.Drawing.Point(113, 465);
+            this.totalCostTitleLabel.Name = "totalCostTitleLabel";
+            this.totalCostTitleLabel.Size = new System.Drawing.Size(69, 13);
+            this.totalCostTitleLabel.TabIndex = 23;
+            this.totalCostTitleLabel.Text = "Total Cost:";
+            // 
+            // totalCostOutputLabel
+            // 
+            this.totalCostOutputLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.totalCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.totalCostOutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalCostOutputLabel.Location = new System.Drawing.Point(188, 464);
+            this.totalCostOutputLabel.Name = "totalCostOutputLabel";
+            this.totalCostOutputLabel.Size = new System.Drawing.Size(100, 15);
+            this.totalCostOutputLabel.TabIndex = 24;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(127, 493);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(128, 23);
+            this.calculateButton.TabIndex = 25;
+            this.calculateButton.Text = "Calculate Estimate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // userWidthTextBox
+            // 
+            this.userWidthTextBox.Location = new System.Drawing.Point(188, 54);
+            this.userWidthTextBox.Name = "userWidthTextBox";
+            this.userWidthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userWidthTextBox.TabIndex = 26;
+            // 
+            // userLengthTextBox
+            // 
+            this.userLengthTextBox.Location = new System.Drawing.Point(188, 86);
+            this.userLengthTextBox.Name = "userLengthTextBox";
+            this.userLengthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userLengthTextBox.TabIndex = 27;
+            // 
+            // userDepthTextBox
+            // 
+            this.userDepthTextBox.Location = new System.Drawing.Point(188, 119);
+            this.userDepthTextBox.Name = "userDepthTextBox";
+            this.userDepthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userDepthTextBox.TabIndex = 28;
+            // 
+            // userMaterialPriceTextBox
+            // 
+            this.userMaterialPriceTextBox.Location = new System.Drawing.Point(188, 155);
+            this.userMaterialPriceTextBox.Name = "userMaterialPriceTextBox";
+            this.userMaterialPriceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userMaterialPriceTextBox.TabIndex = 29;
+            // 
+            // excavationTextBox
+            // 
+            this.excavationTextBox.Location = new System.Drawing.Point(188, 189);
+            this.excavationTextBox.Name = "excavationTextBox";
+            this.excavationTextBox.Size = new System.Drawing.Size(100, 20);
+            this.excavationTextBox.TabIndex = 30;
+            // 
+            // divingBoardTextBox
+            // 
+            this.divingBoardTextBox.Location = new System.Drawing.Point(188, 233);
+            this.divingBoardTextBox.Name = "divingBoardTextBox";
+            this.divingBoardTextBox.Size = new System.Drawing.Size(100, 20);
+            this.divingBoardTextBox.TabIndex = 31;
+            // 
+            // Program1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(384, 558);
+            this.Controls.Add(this.divingBoardTextBox);
+            this.Controls.Add(this.excavationTextBox);
+            this.Controls.Add(this.userMaterialPriceTextBox);
+            this.Controls.Add(this.userDepthTextBox);
+            this.Controls.Add(this.userLengthTextBox);
+            this.Controls.Add(this.userWidthTextBox);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.totalCostOutputLabel);
+            this.Controls.Add(this.totalCostTitleLabel);
+            this.Controls.Add(this.laborCostOutputLabel);
+            this.Controls.Add(this.laborCostTitleLabel);
+            this.Controls.Add(this.excavationCostOutputLabel);
+            this.Controls.Add(this.excavationCostTitleLabel);
+            this.Controls.Add(this.materialCostOutputLabel);
+            this.Controls.Add(this.materialCostTitleLabel);
+            this.Controls.Add(this.cubicYardsOutputLabel);
+            this.Controls.Add(this.cubicYardsTitleLabel);
+            this.Controls.Add(this.divingNoteLabel);
+            this.Controls.Add(this.divingBoardTitleLabel);
+            this.Controls.Add(this.excavationNoteLabel);
+            this.Controls.Add(this.excavationTitleLabel);
+            this.Controls.Add(this.materialPriceTitleLabel);
+            this.Controls.Add(this.maxDepthTitleLabel);
+            this.Controls.Add(this.maxLengthTitleLabel);
+            this.Controls.Add(this.maxWidthTitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Program1";
+            this.Text = "Program 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label maxWidthTitleLabel;
+        private System.Windows.Forms.Label maxLengthTitleLabel;
+        private System.Windows.Forms.Label maxDepthTitleLabel;
+        private System.Windows.Forms.Label materialPriceTitleLabel;
+        private System.Windows.Forms.Label excavationTitleLabel;
+        private System.Windows.Forms.Label excavationNoteLabel;
+        private System.Windows.Forms.Label divingBoardTitleLabel;
+        private System.Windows.Forms.Label divingNoteLabel;
+        private System.Windows.Forms.Label cubicYardsTitleLabel;
+        private System.Windows.Forms.Label cubicYardsOutputLabel;
+        private System.Windows.Forms.Label materialCostTitleLabel;
+        private System.Windows.Forms.Label materialCostOutputLabel;
+        private System.Windows.Forms.Label excavationCostTitleLabel;
+        private System.Windows.Forms.Label excavationCostOutputLabel;
+        private System.Windows.Forms.Label laborCostTitleLabel;
+        private System.Windows.Forms.Label laborCostOutputLabel;
+        private System.Windows.Forms.Label totalCostTitleLabel;
+        private System.Windows.Forms.Label totalCostOutputLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox userWidthTextBox;
+        private System.Windows.Forms.TextBox userLengthTextBox;
+        private System.Windows.Forms.TextBox userDepthTextBox;
+        private System.Windows.Forms.TextBox userMaterialPriceTextBox;
+        private System.Windows.Forms.TextBox excavationTextBox;
+        private System.Windows.Forms.TextBox divingBoardTextBox;
+    }
+}
+
---
+++ Program 1/Program 1/Form1.cs
@@ -0,0 +1,91 @@
+﻿//CIS 199-01
+//Grading ID: K6509
+//Program 1
+//Due Date: 9/30/2021
+//This program takes in multiple user inputs for the dimensions of a proposed pool.
+//Calculations are performed for cubic yards, materials cost, excavation cost, labor cost and total cost.
+//A form is displayed to the user to improve user experience.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_1
+{
+    public partial class Program1 : Form
+    {
+        public Program1()
+        {
+            InitializeComponent();
+        }
+        //event handler for calculate button
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //establishing double and int variables for user input
+            double poolWidth; //user input of width in feet
+            double poolLength; //user input of length in feet
+            double poolDepth; //user input of depth in feet
+            double materialPrice; //user input of price per cubic yard
+            int excavationNeeded; //user input indicating if excavation is needed
+            int divingBoard; //user input indicating if a diving board is needed
+            
+            //establishing constant values for calculations
+            const double wasteAddition = .10; //10% extra material
+            const double excavationPrice = 15; //$15 per cubic yard
+
+            //establishing variables for calculations
+            double cubicYardOutput; //calculated cubic yards
+            double materialCostsOutput; //calculated material costs
+            double extraMaterials; //calculated extra material costs
+            double excavationCostOutput = 0; //initializing a value for excavation, whether it is needed or not
+            double laborCostOutput; //calculated labor costs
+            double totalCostOutput; //calculated total cost of project
+            double divingBoardCost = 50; //setting value for fixed cost of diving board
+
+            //converting user inputs to compatible data types
+            poolWidth = Convert.ToDouble(userWidthTextBox.Text); //double value for width
+            poolLength = Convert.ToDouble(userLengthTextBox.Text); //double value for length
+            poolDepth = Convert.ToDouble(userDepthTextBox.Text); //double value for depth
+            materialPrice = Convert.ToDouble(userMaterialPriceTextBox.Text); //double value for price
+            excavationNeeded = Convert.ToInt32(excavationTextBox.Text); //integer for excavation condition
+            divingBoard = Convert.ToInt32(divingBoardTextBox.Text); //integer for diving board condition
+
+            //calculating cubic yards, converting cubic feet to cubic yards
+            double cubicYardConversion = (poolWidth * poolLength * poolDepth) / 27;
+
+            //calculations for output to user
+            cubicYardOutput = cubicYardConversion; //setting output as conversion value for extra material calculation
+            extraMaterials = (cubicYardOutput * wasteAddition); //calculating extra materials for project (cubic yards)
+            materialCostsOutput = (cubicYardOutput * materialPrice) + (extraMaterials * materialPrice); //calculating total material costs for project
+            laborCostOutput = cubicYardOutput * 3.25; //calculating labor cost for project
+
+            //if statement to meet condition if user needs excavation, else statement not needed in this case
+            if(excavationNeeded == 1) //1 indicating excavation is needed
+            {
+                excavationCostOutput = cubicYardOutput * excavationPrice; //calculating excavation cost
+            }
+
+            //if statement to meet condition if user needs a diving board, else statement not needed in this case
+            if(divingBoard == 1) //1 indicating diving baord is needed
+            {
+                laborCostOutput += divingBoardCost; //calculating diving board cost, adding to labor
+            }
+            
+            //calculating total cost of project
+            totalCostOutput = materialCostsOutput + excavationCostOutput + laborCostOutput;
+
+            //outputting calculated values to user with different formatting depending on desired value
+            cubicYardsOutputLabel.Text = cubicYardOutput.ToString("0.0"); //output for cubic yards of pool only, not including extra materials
+            materialCostOutputLabel.Text = materialCostsOutput.ToString("C"); //output for total material cost
+            excavationCostOutputLabel.Text = excavationCostOutput.ToString("C"); //output for excavation cost
+            laborCostOutputLabel.Text = laborCostOutput.ToString("C"); //output for total labor cost
+            totalCostOutputLabel.Text = totalCostOutput.ToString("C"); //output for total project cost
+        }
+    }
+}
